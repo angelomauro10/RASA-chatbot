a9a1ebf1b752d83a9e07e39b6e978dee678cb7f5
Angelo Mauro Calderón <angelomauro_10@hotmail.com>
2023-05-30 01:29:33 -0300

    Update actions.py

--- actions.py
+++ actions.py
@@ -30,45 +30,14 @@ class ActionSetSlot(Action):
        for index, row in iterador:
            
            nombre = str(row['nombre'])
-           idcliente = str(row['idcliente']).replace('BSR_TARDIA', 'Banco Santander Rio').replace(
-               'FRANCES_2012', 'Banco BBVA Francés').replace('AFM_HSBC', 'Banco HSBC')
+           idcliente = str(row['idcliente']).replace('BANK1', 'International Bank 1').replace(
+               'BANK2', 'National Bank 2').replace('BANK3', 'Banking Society 3')
            saldo_total = str(row['saldo_total'])
 
 
            return [SlotSet('nombre', nombre),
                    SlotSet('idcliente', idcliente),
                    SlotSet('saldo_total', saldo_total)]
-
-
-#   def run(self, dispatcher: CollectingDispatcher,
-#           tracker: Tracker,
-#           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#       conn = pymssql.connect(server='111.111.111.111', user='abcdef', password='1234567', database='ddbb')
-#
-#       cursor = conn.cursor()
-#
-#       #sql = 'SELECT  * FROM dbo.database_name'
-#
-#       cursor.execute('SELECT * FROM dbo.database_name')
-#
-#       row = cursor.fetchall()
-#
-#       while row:
-#
-#
-#           nombre = str(row[0][1])
-#           idcliente = str(row[0][2]).replace('BSR_TARDIA', 'Banco Santander Rio').replace(
-#               'FRANCES_2012', 'Banco BBVA Francés').replace('AFM_HSBC', 'Banco HSBC')
-#           saldo_total = str(row[0][0])
-#
-#           row = cursor.fetchall()
-#
-#           return [SlotSet('nombre', nombre),
-#                   SlotSet('idcliente', idcliente),
-#                   SlotSet('saldo_total', saldo_total)]
-
-
 
 
 
